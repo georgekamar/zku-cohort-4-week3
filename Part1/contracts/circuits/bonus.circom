800a86bbdfc75fd9ead36446b075c321e48602f8
georgeKamar <gkamar97@gmail.com>
2022-07-25 19:47:00 +0300

    assignment 3 complete

--- Part1/contracts/circuits/bonus.circom
+++ Part1/contracts/circuits/bonus.circom
@@ -1,1 +1,163 @@
-// [bonus] implement an example game from part d
+// // [bonus] implement an example game from part d
+
+// pragma circom 2.0.0;
+
+// // [assignment] implement a variation of mastermind from https://en.wikipedia.org/wiki/Mastermind_(board_game)#Variation as a circuit
+
+// // Implementing Guess Who
+// // 5 traits, 8 questions
+
+// include "../../node_modules/circomlib/circuits/comparators.circom";
+// include "../../node_modules/circomlib/circuits/bitify.circom";
+// include "../../node_modules/circomlib/circuits/poseidon.circom";
+
+
+// // calculate total and quin selector reference taken from: https://github.com/darkforest-eth/circuits/blob/master/perlin/
+
+// template CalculateTotal(n) {
+//     signal input in[n];
+//     signal output out;
+
+//     signal sums[n];
+
+//     sums[0] <== in[0];
+
+//     for (var i = 1; i < n; i++) {
+//         sums[i] <== sums[i-1] + in[i]
+//     }
+
+//     out <== sums[n-1];
+// }
+
+// template QuinSelector(choices) {
+//     signal input in[choices];
+//     signal input index;
+//     signal output out;
+
+//     // Ensure that index < choices
+//     component lessThan = LessThan(4);
+//     lessThan.in[0] <== index;
+//     lessThan.in[1] <== choices;
+//     lessThan.out === 1;
+
+//     component calcTotal = CalculateTotal(choices);
+//     component eqs[choices];
+
+//     // For each item, check whether its index equals the input index.
+//     for (var i = 0; i < choices; i ++) {
+//         eqs[i] = IsEqual();
+//         eqs[i].in[0] <== i;
+//         eqs[i].in[1] <== index;
+
+//         // eqs[i].out is 1 if the index matches. As such, at most one input to
+//         // calcTotal is not 0.
+//         calcTotal.in[i] <== eqs[i].out * in[i];
+//     }
+
+//     // Returns 0 + 0 + 0 + item
+//     out <== calcTotal.out;
+// }
+
+
+// template GuessWho() {
+
+
+//   // Public inputs
+//   signal input traitIndexQuestion;
+//   signal input traitIndexAnswer;
+//   signal input characterGuessNameIndex;
+//   signal input characterGuessAnswer;
+//   signal input pubSolnHash;
+
+//   // Private inputs
+//   signal input privCharacterNameIndex;
+//   signal input privTraitA;
+//   signal input privTraitB;
+//   signal input privTraitC;
+//   signal input privTraitD;
+//   signal input privTraitE;
+//   signal input privSalt;
+
+//   // Output
+//   signal output solnHashOut;
+
+//   var soln[5] = [privTraitA, privTraitB, privTraitC, privTraitD, privTraitE];
+//   var j = 0;
+
+//   // Create a constraint that the solution traits 0 or more (more being "has the trait", 0 being "does not have the trait")
+//   // and that questioned trait index is less than 5
+
+//   component lessThan = LessThan(3);
+//   lessThan.in[0] <== traitIndexQuestion;
+//   lessThan.in[1] <== 5;
+//   lessThan.out === 1;
+
+//   component greaterEqThan[5];
+
+//   for (j=0; j<5; j++) {
+//       greaterEqThan[j] = GreaterEqThan(32);
+//       greaterEqThan[j].in[0] <== soln[j];
+//       greaterEqThan[j].in[1] <== 0;
+//       greaterEqThan[j].out === 1;
+//   }
+
+//   // Create a constraint the the answer to the character's guessed
+//   // name (if there is one != -1) is correct
+//   // Answer: 1 for correct guessed name, 0 for incorrect guessed name
+
+//   component isEqual0 = IsEqual();
+//   isEqual0.in[0] <== characterGuessName;
+//   isEqual0.in[1] <== -1;
+
+//   component isEqual1 = IsEqual();
+//   isEqual1.in[0] <== characterGuessAnswer;
+//   isEqual1.in[1] <== -1;
+
+//   component isEqual2 = IsEqual();
+
+//   component quinSelector = QuinSelector(5);
+
+//   if(isEqual1.out == 0){
+
+//     // Create a contraint that the index answer is incorrect
+
+//     quinSelector.in <== soln;
+//     quinSelector.index <== traitIndexQuestion;
+//     quinSelector.out === traitIndexAnswer;
+
+//   }
+
+//   // Verify that the hash of the private solution matches pubSolnHash
+//   component poseidon = Poseidon(7);
+//   poseidon.inputs[0] <== privSalt;
+//   poseidon.inputs[1] <== privTraitA;
+//   poseidon.inputs[2] <== privTraitB;
+//   poseidon.inputs[3] <== privTraitC;
+//   poseidon.inputs[4] <== privTraitD;
+//   poseidon.inputs[5] <== privTraitE;
+//   poseidon.inputs[6] <== privCharacterName;
+
+//   solnHashOut <== poseidon.out;
+// //  pubSolnHash === solnHashOut;
+
+// }
+
+// component main = GuessWho();
+
+// /*
+
+// INPUT = {
+//     "privTraitA": "1",
+//     "privTraitB": "0",
+//     "privTraitC": "0",
+//     "privTraitD": "1",
+//     "privTraitE": "0",
+//     "privSalt": "123",
+//     "traitIndexQuestion": "0",
+//     "traitIndexAnswer": "1",
+//     "characterGuessName": "-1",
+//     "characterGuessAnswer": "-1",
+//     "pubSolnHash": "3157727679588526826632910333096458056600411126500422373775748430124589368926"
+// }
+
+// */
--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,197 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const { expect, assert } = require("chai");
+// const { ethers } = require("hardhat");
+const { groth16, plonk } = require("snarkjs");
+
+const wasm_tester = require("circom_tester").wasm;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+describe("MastermindVariation", function () {
+    this.timeout(100000000);
+
+    it("Circuit should not throw an error: public solution hash is correct using poseidon with the private inputs, number of hits and blows are correct", async function () {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+
+        const INPUT = {
+            "pubSolnHash": "3157727679588526826632910333096458056600411126500422373775748430124589368926",
+            "pubGuessA": 2,
+            "pubGuessB": 3,
+            "pubGuessC": 1,
+            "pubGuessD": 7,
+            "pubGuessE": 6,
+            "pubNumHit": 0,
+            "pubNumBlow": 3,
+            "privSolnA": 1,
+            "privSolnB": 2,
+            "privSolnC": 3,
+            "privSolnD": 4,
+            "privSolnE": 5,
+            "privSalt": 123
+        }
+
+        try{
+
+          const witness = await circuit.calculateWitness(INPUT, true);
+
+          assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
+          assert(Fr.eq(Fr.e(witness[1]),Fr.e("3157727679588526826632910333096458056600411126500422373775748430124589368926")));
+          assert(Fr.eq(Fr.e(witness[2]),Fr.e(2)));
+          assert(Fr.eq(Fr.e(witness[3]),Fr.e(3)));
+          assert(Fr.eq(Fr.e(witness[4]),Fr.e(1)));
+          assert(Fr.eq(Fr.e(witness[5]),Fr.e(7)));
+          assert(Fr.eq(Fr.e(witness[6]),Fr.e(6)));
+          assert(Fr.eq(Fr.e(witness[7]),Fr.e(0)));
+          assert(Fr.eq(Fr.e(witness[8]),Fr.e(3)));
+          assert(Fr.eq(Fr.e(witness[9]),Fr.e(1)));
+          assert(Fr.eq(Fr.e(witness[10]),Fr.e(2)));
+          assert(Fr.eq(Fr.e(witness[11]),Fr.e(3)));
+          assert(Fr.eq(Fr.e(witness[12]),Fr.e(4)));
+          assert(Fr.eq(Fr.e(witness[13]),Fr.e(5)));
+          assert(Fr.eq(Fr.e(witness[14]),Fr.e(123)));
+
+
+        }catch(e){
+          console.log('ERROR');
+          console.log(e);
+          assert(false);
+        }
+
+
+    });
+
+
+    it("Circuit should throw an error: public solution hash is correct using poseidon with the private inputs, but the number of blows is inaccurate", async function () {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+
+        const INPUT = {
+            "pubSolnHash": "3157727679588526826632910333096458056600411126500422373775748430124589368926",
+            "pubGuessA": 2,
+            "pubGuessB": 3,
+            "pubGuessC": 1,
+            "pubGuessD": 7,
+            "pubGuessE": 6,
+            "pubNumHit": 0,
+            "pubNumBlow": 2,
+            "privSolnA": 1,
+            "privSolnB": 2,
+            "privSolnC": 3,
+            "privSolnD": 4,
+            "privSolnE": 5,
+            "privSalt": 123
+        }
+
+        try{
+
+          const witness = await circuit.calculateWitness(INPUT, true);
+          assert(false);
+
+        }catch(e){
+          assert(true)
+        }
+
+
+    });
+
+    it("Circuit should throw an error: public solution hash is incorrect using poseidon with the private inputs even though the number of hits and blows are accurate", async function () {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+
+        const INPUT = {
+            "pubSolnHash": "1157727679588526826632910333096458056600411126500422373775748430124589368926",
+            "pubGuessA": 2,
+            "pubGuessB": 3,
+            "pubGuessC": 1,
+            "pubGuessD": 7,
+            "pubGuessE": 6,
+            "pubNumHit": 0,
+            "pubNumBlow": 3,
+            "privSolnA": 1,
+            "privSolnB": 2,
+            "privSolnC": 3,
+            "privSolnD": 4,
+            "privSolnE": 5,
+            "privSalt": 123
+        }
+
+        try{
+
+          const witness = await circuit.calculateWitness(INPUT, true);
+          assert(false);
+
+        }catch(e){
+          assert(true)
+        }
+
+
+    });
+
+    it("Circuit should throw an error: public solution hash is incorrect using poseidon with the private inputs even though the number of hits and blows are accurate", async function () {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+
+        const INPUT = {
+            "pubSolnHash": "3157727679588526826632910333096458056600411126500422373775748430124589368926",
+            "pubGuessA": 2,
+            "pubGuessB": 3,
+            "pubGuessC": 1,
+            "pubGuessD": 7,
+            "pubGuessE": 6,
+            "pubNumHit": 0,
+            "pubNumBlow": 3,
+            "privSolnA": 1,
+            "privSolnB": 2,
+            "privSolnC": 3,
+            "privSolnD": 4,
+            "privSolnE": 5,
+            "privSalt": 1234
+        }
+
+        try{
+
+          const witness = await circuit.calculateWitness(INPUT, true);
+          assert(false);
+
+        }catch(e){
+          assert(true)
+        }
+
+
+    });
+
+
+    it("Circuit should throw an error: there are duplicate colors in the breaker's guess", async function () {
+        const circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+
+        const INPUT = {
+            "pubSolnHash": "3157727679588526826632910333096458056600411126500422373775748430124589368926",
+            "pubGuessA": 2,
+            "pubGuessB": 3,
+            "pubGuessC": 1,
+            "pubGuessD": 6,
+            "pubGuessE": 6,
+            "pubNumHit": 0,
+            "pubNumBlow": 2,
+            "privSolnA": 1,
+            "privSolnB": 2,
+            "privSolnC": 3,
+            "privSolnD": 4,
+            "privSolnE": 5,
+            "privSalt": 123
+        }
+
+        try{
+
+          const witness = await circuit.calculateWitness(INPUT, true);
+          assert(false);
+
+        }catch(e){
+          assert(true)
+        }
+
+
+    });
+
+});
